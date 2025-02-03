04b964a8d9adbf255e84e554dfc2891fdfa10d5a
Gustavo Rios <gerquevedo@unsm.edu.pe>
2025-02-03 12:11:59 -0500

    Logica hecha a nivel js (console.log)

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+
+function limpiarcaja(){
+    document.querySelector('#amigo').value='';
+}
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    if (nombre == ""){
+        alert("Nombre inválido, intentelo nuevamente");
+    } else {
+        if (nombresAmigos.includes(nombre)){
+            alert("Nombre ya existe");
+            limpiarcaja();
+        } else {
+            nombresAmigos.push(nombre);
+            console.log(nombresAmigos);
+            limpiarcaja();
+        }
+        
+    }
+    
+}
+
+
+
+function sortearAmigo(){
+    let  amigosorteado = Math.floor(Math.random()*(nombresAmigos.length));
+    console.log(amigosorteado);
+    console.log(nombresAmigos[amigosorteado]);
+
+}
